80c4e56915c7adde52b954899cac3e9515626510
Ryder Roedel <Ryder Roedel@DESKTOP-I3OEELD>
2018-11-13 17:41:18 -0800

    Here's the project

---
+++ FamilyTree/Backend/FamilyBackend.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FamilyTree.Models;
+using FamilyTree.Models.Enums;
+
+
+namespace FamilyTree.Backend
+{
+    public class FamilyBackend 
+    {
+            public FamilyBackend()
+            {
+                MyFam = new FamilyModel();
+            }
+
+            public bool setMe(string FirstName, string LastName, DateTime DOB)
+            {
+                foreach (PersonModel member in MyFam.FMem)
+                {
+                    if (member.IsToMe == RelationEnum.Self)
+                        return false;
+                }
+
+                MyFam.FMem.AddLast(value: new PersonModel(FirstName, LastName, DOB, RelationEnum.Self));
+
+                return true;
+            }
+
+            public bool addMember(PersonModel nextMember)
+            {
+                MyFam.FMem.AddLast(nextMember);
+
+                return true;
+            }
+
+        public FamilyModel MyFam;
+    }
+}
