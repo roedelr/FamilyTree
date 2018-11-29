06afc4f5716947e210f1aa4335f7556cfb033083
Patrick Shipley <shipley3@seattleu.edu>
2018-11-28 16:43:13 -0800

    UserModel attributes

--- FamilyTree/Models/UserModel.cs
+++ FamilyTree/Models/UserModel.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace FamilyTree.Models
 {
-    public class UserModel : Controller
+    public class UserModel
     {
-        // GET: UserModel
-        public ActionResult Index()
+        [DisplayName("Username")]
+        [Required(ErrorMessage = "Enter a Username")]
+        public string UserName { get; set; }
+
+        [DisplayName("Username")]
+        [Required(ErrorMessage = "Enter a Username")]
+        [DataType(DataType.EmailAddress)]
+        public string UserEmail { get; set; }
+
+        [DisplayName("Username")]
+        [Required(ErrorMessage = "Enter a Username")]
+        public string UserPassword { get; set; }
+
+        public UserModel()
         {
-            return View();
+            UserName = "user1";
+            UserEmail = "user@email.com";
+            UserPassword = "password123";
+        }
+
+        public UserModel(string UserName, EmailAddressAttribute UserEmail, string UserPassword)
+        {
+            this.UserName = UserName;
+            this.UserEmail = UserEmail;
+            this.DOB = DOB;
+            IsToMe = Relation;
         }
     }
 }
