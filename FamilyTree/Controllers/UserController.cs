d35a9f4f0ead4bbc9d214d4912b806937b9b1a02
Patrick Shipley <shipley3@seattleu.edu>
2018-11-28 17:18:12 -0800

    User Controller, SignIn View

--- FamilyTree/Controllers/UserController.cs
+++ FamilyTree/Controllers/UserController.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FamilyTree.Models;
 
 namespace FamilyTree.Controllers
 {
     public class UserController : Controller
     {
+        public static UserModel user = new UserModel();
+
         // GET: User
         public ActionResult Index()
         {
+            return View();
+        }
+
+        // GET: /Service/SignIn
+        [HttpGet]
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        //
+        // POST: /User/SignIn
+
+        [HttpPost]
+        public ActionResult SignIn([Bind(Include =
+            "UserName," +
+            "UserEmail," +
+            "UserPassword," +
+            "")] UserModel NextMember)
+        {
+
+            if (NextMember == user)
+            {
+                return RedirectToAction("Family/Index");
+            }
+
             return View();
         }
     }
